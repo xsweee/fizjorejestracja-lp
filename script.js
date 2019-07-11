1da00ddb7d09e961f7981b7059b99ae24f005faf
krystianslovik <yam@tuta.io>
2019-07-11 14:30:29 +0200

    podstrony + tylko hd/fhd

---
+++ script.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function(){
+	 dlaKogo	=	document.getElementById("dlaKogo"),
+	 jakDziala 	=	document.getElementById("jakDziala"),
+	 oNas 		=	document.getElementById("oNas"),
+	 kontakt 	=	document.getElementById("kontakt"),
+	 oplaty		=	document.getElementById("oplaty"),
+	 logo 		=	document.getElementById("logo");
+
+	dlaKogo.addEventListener("click", function(){
+		console.log("dla kogo");
+		document.location.href = "file:///C:/Users/kryst/Desktop/fizjorejestracja/fizjorejestracja/dla-kogo.htm";
+	})
+	jakDziala.addEventListener("click", function(){
+			console.log("dla kogo");
+			document.location.href = "file:///C:/Users/kryst/Desktop/fizjorejestracja/fizjorejestracja/jak-dziala.htm";
+		})
+	oNas.addEventListener("click", function(){
+			console.log("dla kogo");
+			document.location.href = "file:///C:/Users/kryst/Desktop/fizjorejestracja/fizjorejestracja/o-nas.htm";
+		})
+	kontakt.addEventListener("click", function(){
+			console.log("dla kogo");
+			document.location.href = "file:///C:/Users/kryst/Desktop/fizjorejestracja/fizjorejestracja/kontakt.htm";
+		})
+	oplaty.addEventListener("click", function(){
+			console.log("dla kogo");
+			document.location.href = "file:///C:/Users/kryst/Desktop/fizjorejestracja/fizjorejestracja/oplaty.htm";
+		})
+	logo.addEventListener("click", function(){
+				console.log("dla kogo");
+				document.location.href = "file:///C:/Users/kryst/Desktop/fizjorejestracja/fizjorejestracja/index.htm";
+			})
+
+
+
+})
